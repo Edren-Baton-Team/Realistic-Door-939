723268134349ec9f820393177feb6c2e6405e989
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2022-12-04 20:09:55 +0400

    Add files via upload

---
+++ Realistic Door 939/Config.cs
@@ -0,0 +1,17 @@
+﻿using Exiled.API.Interfaces;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Realistic_Door_939.Plugin
+{
+    public class Config : IConfig
+    {
+        [Description("Is the plugin enabled?")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Chance of knocking out the door? from 0 to 100")]
+        public float Chance { get; set; } = 99;
+        [Description("The time when it can be repeated (cooldown)")]
+        public float Timing { get; set; } = 20f;
+
+    }
+}
---
+++ Realistic Door 939/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using MEC;
+using System.Collections.Generic;
+
+namespace Realistic_Door_939.Plugin
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author => "Rysik5318";
+        public override string Name => "Realistic Destroys Door 939";
+        public override string Prefix => "Realistic Destroys Door 939";
+        public override System.Version Version => new System.Version(1, 0, 0);
+        public static Plugin Singleton;
+        public List<string> time939 = new List<string>();
+        public override void OnEnabled()
+        {
+            Singleton = this;
+            Exiled.Events.Handlers.Player.InteractingDoor += OnInteractingDoor;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Singleton = null;
+            Exiled.Events.Handlers.Player.InteractingDoor -= OnInteractingDoor;
+            base.OnDisabled();
+        }
+
+        public void OnInteractingDoor(InteractingDoorEventArgs ev)
+        {
+            string Id = ev.Player.UserId;
+            if (!ev.Door.IsKeycardDoor && ev.Player.Role.Type.Is939() && !time939.Contains(Id))
+            {
+                if (UnityEngine.Random.Range(0, 100) > Config.Chance)
+                {
+                    time939.Add(Id);
+                    ev.Door.BreakDoor();
+                }
+                else
+                {
+                    time939.Add(Id);
+                }
+                Timing.CallDelayed(Config.Timing, () => { time939.Remove(Id); });
+            }
+        }
+    }
+}
